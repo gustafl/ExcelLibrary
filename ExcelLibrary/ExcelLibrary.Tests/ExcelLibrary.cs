de7cb43e79ec19176849af8561857d2e7ef28cee
gustafl <gustafl@bahnhof.se>
2015-02-19 23:15:58 +0100

    Added unit test project.

---
+++ ExcelLibrary/ExcelLibrary.Tests/ExcelLibrary.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExcelLibrary.Tests
+{
+    [TestClass]
+    public class ExcelLibrary
+    {
+        private const string FILE = @"C:\Users\Gustaf\Desktop\test2.xlsx";
+
+        private Workbook workbook = null;
+        private Workbook workbookWithOptions = null;
+
+        [TestInitialize]
+        [TestCategory("Workbook")]
+        public void Initialize()
+        {
+            this.workbook = new Workbook();
+            this.workbook.Open(FILE);
+
+            WorkbookOptions options = new WorkbookOptions();
+            options.IncludeHidden = true;
+            this.workbookWithOptions = new Workbook();
+            this.workbookWithOptions.Open(FILE, options);
+        }
+
+        [TestMethod]
+        [TestCategory("Workbook")]
+        public void OpenWorkbook()
+        {
+            Assert.IsInstanceOfType(this.workbook, typeof(Workbook));
+        }
+
+        [TestMethod]
+        [TestCategory("Workbook")]
+        public void OpenWorkbookWithOptions()
+        {
+            Assert.IsInstanceOfType(this.workbookWithOptions, typeof(Workbook));
+        }
+
+        [TestMethod]
+        [TestCategory("Workbook")]
+        public void CountSheetsExcludingHidden()
+        {
+            var sheets = this.workbook.Sheets;
+            Assert.AreEqual(3, sheets.Count());
+        }
+
+        [TestMethod]
+        [TestCategory("Workbook")]
+        public void CountSheetsIncludingHidden()
+        {
+            var sheets = this.workbookWithOptions.Sheets;
+            Assert.AreEqual(4, sheets.Count());
+        }
+
+        [TestMethod]
+        [TestCategory("Workbook")]
+        public void GetWorkbookFile()
+        {
+            Assert.AreEqual(FILE, this.workbook.File);
+        }
+
+        [TestMethod]
+        [TestCategory("Workbook")]
+        public void GetWorkbookOptionsWithOptionsLoaded()
+        {
+            WorkbookOptions options = this.workbookWithOptions.Options;
+            Assert.AreEqual(true, options.IncludeHidden);
+        }
+
+        [TestMethod]
+        [TestCategory("Workbook")]
+        public void GetWorkbookOptionsWithNoOptionsLoaded()
+        {
+            WorkbookOptions options = this.workbook.Options;
+            Assert.AreEqual(false, options.IncludeHidden);
+        }
+
+        [TestMethod]
+        [TestCategory("Workbook")]
+        public void GetSharedStringsCount()
+        {
+            Assert.AreEqual(1, this.workbook.SharedStrings.Count);
+        }
+    }
+}
--- ExcelLibrary/ExcelLibrary.Tests/Properties/AssemblyInfo.cs
+++ ExcelLibrary/ExcelLibrary.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExcelLibrary")]
+[assembly: AssemblyTitle("ExcelLibrary.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExcelLibrary")]
+[assembly: AssemblyProduct("ExcelLibrary.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6ed739b8-302f-4b4c-8abf-e765bfda0921")]
+[assembly: Guid("80efe93d-618b-4c10-99ea-2746137a30b3")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("ExcelLibrary.Tests")]
